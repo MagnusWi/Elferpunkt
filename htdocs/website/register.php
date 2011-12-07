b2276953f4b209def8639632baae8a07662d9b94
Mr. Senkstyla <besoft@gmx.de>
2011-12-07 11:30:01 +0100

    login/register implementation

--- htdocs/website/register.php
+++ htdocs/website/register.php
@@ -1,9 +1,12 @@
 <?php include('_inc.header.php') ?>
 
+<?= $params ?>
+
+
 <fb:registration
   fields="name,first_name,last_name,gender,email,password,captcha"
   redirect-uri="http://elferpunkt.de/register/complete"
-  width="530">
+  width="600">
 </fb:registration>
 
 <?php include('_inc.footer.php') ?>
