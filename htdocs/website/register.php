090c89c15537a5ec11151a8e5795b370b7c4467a
Mr. Senkstyla <besoft@gmx.de>
2011-12-07 10:38:04 +0100

    login/register implementation

---
+++ htdocs/website/register.php
@@ -0,0 +1,9 @@
+<?php include('_inc.header.php') ?>
+
+<fb:registration
+  fields="name,birthday,gender,location,email"
+  redirect-uri="https://developers.facebook.com/tools/echo/"
+  width="530">
+</fb:registration>
+
+<?php include('_inc.footer.php') ?>
